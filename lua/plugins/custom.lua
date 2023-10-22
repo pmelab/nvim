cd0c29b9e716b7081b086587cfb1d2762f7692cb
Philipp Melab <philipp.melab@gmail.com>
2023-10-22 18:43:34 +0200

    update packages

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -66,6 +66,7 @@ return {
   {
     "nvim-neotest/neotest",
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "marilari88/neotest-vitest",
       "olimorris/neotest-phpunit",
     },
