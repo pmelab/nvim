ba64aa519a2b10faa8ec2bde227aa68c7f52a3ee
Philipp Melab <philipp.melab@gmail.com>
2023-11-05 05:28:27 +0100

    enable mini files

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -13,6 +13,22 @@ end
 
 return {
   {
+    "echasnovski/mini.files",
+    opts = {
+
+      windows = {
+        preview = true,
+        width_focus = 30,
+        width_preview = 50,
+      },
+      options = {
+        -- Whether to use for editing directories
+        -- Disabled by default in LazyVim because neo-tree is used for that
+        use_as_default_explorer = true,
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
